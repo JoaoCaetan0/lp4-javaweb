0a06fa760cefb0f0c65ba8d18987ceca7eac1a24
Aluno <joao.caetano@estudante.ifms.edu.br>
2022-06-03 14:55:00 -0400

    Security

---
+++ src/main/java/com/lp4/caetanoweb/ConfiguracaoVisao.java
@@ -0,0 +1,17 @@
+package com.lp4.caetanoweb;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class ConfiguracaoVisao implements WebMvcConfigurer {
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/auth/admin/admin-index").setViewName("/auth/admin/admin-index");
+		registry.addViewController("/auth/user/user-index").setViewName("/auth/user/user-index");
+		registry.addViewController("/auth/gerente/gerente-index").setViewName("/auth/gerente/gerente-index");
+		registry.addViewController("/auth/auth-acesso-negado").setViewName("/auth/auth-acesso-negado");
+	}
+
+}
